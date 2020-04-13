c925cc0bf1d1f1ac799b41865bfe9d84d4a15888
JessicaBlau <jblau1993@gmail.com>
2020-04-13 22:03:25 +0300

    "update"

---
+++ partA/hello_ariel.c
@@ -0,0 +1,6 @@
+#include "hello_ariel.h"
+#include <stdio.h>
+
+void sayHello(){
+    printf("hello ariel\n");
+}
---
+++ partA/main1_3.c
@@ -0,0 +1,11 @@
+#include "hello_ariel.h"
+#include <stdio.h>
+#include <stdbool.h>
+#include <dlfcn.h>
+#include <stddef.h>
+
+void ( * messgae)();
+
+bool initLibrary(){
+    
+}
