c569370fbf35939c3e52aaea2b02318944b9239f
JessicaBlau <jblau1993@gmail.com>
2020-04-15 19:38:24 +0300

    parA done

--- partA/main1_2.c
+++
@@ -1,7 +0,0 @@
-#include "hello_ariel.h"
-#include <stdio.h>
-
-int main(){
-    sayHello();
-    return 0;
-}
--- partA/main1_3.c
+++ partA/main1_3.c
@@ -4,8 +4,26 @@
 #include <dlfcn.h>
 #include <stddef.h>
 
-void ( * messgae)();
+void ( * msg)();
 
 bool initLibrary(){
-    
+    void *hdl = dlopen("lib.so", RTLD_LAZY);
+    if(hdl == NULL){
+        return false;
+    }
+    msg = (void(*))dlsym(hdl,"sayHello");
+    if(msg == NULL){
+        return false;
+    }
+    return true;
+}
+
+int main(){
+    if(initLibrary()){
+        msg();
+    }
+    else{
+        printf("The library was not loaded\n");
+    }
+    return 0;
 }
